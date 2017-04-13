bc67941919266b07868f7f1213f0cd3c6f1c9984
wxcican <wxcican@qq.com>
2017-04-13 17:35:36 +0800

    初次提交，资源导入

---
+++ app/src/main/java/com/fuicuiedu/xc/easyshop_20170413/main/MainActivity.java
@@ -0,0 +1,15 @@
+package com.fuicuiedu.xc.easyshop_20170413.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.fuicuiedu.xc.easyshop_20170413.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
