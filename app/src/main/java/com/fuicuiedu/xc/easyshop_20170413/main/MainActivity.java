693d55b139e3d19c57d3364da79db5721633e9d0
wxcican <wxcican@qq.com>
2017-04-14 15:19:47 +0800

    项目搭建，简单实现。

--- app/src/main/java/com/fuicuiedu/xc/easyshop_20170413/main/MainActivity.java
+++ app/src/main/java/com/fuicuiedu/xc/easyshop_20170413/main/MainActivity.java
@@ -1,5 +1,8 @@
 package com.fuicuiedu.xc.easyshop_20170413.main;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,14 +11,17 @@ import android.widget.TextView;
 
 import com.fuicuiedu.xc.easyshop_20170413.R;
 import com.fuicuiedu.xc.easyshop_20170413.commons.ActivityUtils;
+import com.fuicuiedu.xc.easyshop_20170413.main.me.MeFragment;
+import com.fuicuiedu.xc.easyshop_20170413.main.shop.ShopFragment;
 
 import butterknife.BindView;
 import butterknife.BindViews;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindViews({R.id.tv_shop,R.id.tv_message,R.id.tv_mail_list,R.id.tv_me})
+    @BindViews({R.id.tv_shop, R.id.tv_message, R.id.tv_mail_list, R.id.tv_me})
     TextView[] textViews;
     @BindView(R.id.main_toobar)
     Toolbar toolbar;
@@ -35,12 +41,85 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         activityUtils = new ActivityUtils(this);
 
+        //初始化视图
+        init();
+    }
+
+    private void init() {
+        viewPager.setAdapter(unLoginAdapter);
+
+        //刚进来默认选择市场
+        textViews[0].setSelected(true);
+        //控制TextView的图标，通过ViewPager滑动监听
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                //textView全部为选择
+                for (TextView textView : textViews) {
+                    textView.setSelected(false);
+                }
+
+                //更改title，设置选择效果
+                tv_title.setText(textViews[position].getText());
+                textViews[position].setSelected(true);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
+    }
+
+    // TODO: 2017/4/14 0014 用户已经登录的适配器
+    //用户未登录时的适配器
+    private FragmentStatePagerAdapter unLoginAdapter = new FragmentStatePagerAdapter(getSupportFragmentManager()) {
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                //市场
+                case 0:
+                    return new ShopFragment();
+                //消息
+                case 1:
+                    return new UnLoginFragment();
+                //通讯录
+                case 2:
+                    return new UnLoginFragment();
+                //我的
+                case 3:
+                    return new MeFragment();
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 4;
+        }
+    };
+
+    //textView点击事件
+    @OnClick({R.id.tv_shop, R.id.tv_message, R.id.tv_mail_list, R.id.tv_me})
+    public void onClick(TextView textView) {
+        for (int i = 0; i < textViews.length; i++) {
+            textViews[i].setSelected(false);
+            textViews[i].setTag(i);
+        }
+        //设置选择效果
+        textView.setSelected(true);
+        //不要平滑效果，第二个参数为false
+        viewPager.setCurrentItem((int) textView.getTag(), false);
+        tv_title.setText(textViews[(int) textView.getTag()].getText());
     }
 
     @Override
     public void onBackPressed() {
         //点击2次返回，退出程序
-        if (!isExit){
+        if (!isExit) {
             isExit = true;
             activityUtils.showToast("在摁一次退出程序！");
             //如果两秒内再次点击返回则退出
@@ -50,7 +129,7 @@ public class MainActivity extends AppCompatActivity {
                     isExit = false;
                 }
             }, 2000);
-        }else{
+        } else {
             finish();
         }
     }
---
+++ app/src/main/java/com/fuicuiedu/xc/easyshop_20170413/main/me/MeFragment.java
@@ -0,0 +1,24 @@
+package com.fuicuiedu.xc.easyshop_20170413.main.me;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.fuicuiedu.xc.easyshop_20170413.R;
+
+/**
+ * Created by Administrator on 2017/4/14 0014.
+ */
+
+public class MeFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_me,container,false);
+        return view;
+    }
+}
